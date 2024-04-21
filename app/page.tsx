263b9e612b40d1ef51c9709044928117af923597
dishika.goel <dishika.goel@drexel.edu>
2024-04-21 02:42:18 -0400

    add dashboard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import {Code} from "lucide-react";
 import {CodeWindow} from "@/components/code-window";
+import {Dashboard} from "@/components/dashboard";
 
 export default function Home() {
   return (
     <div>
-      <CodeWindow />
+      <Dashboard />
     </div>
   );
 }
